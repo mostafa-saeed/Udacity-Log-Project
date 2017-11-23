898b6a6bd0ee7b46c4f0bc1d1016421a61fd0a1b
Mostafa Saeed <mostafa.saeed543@gmail.com>
2017-11-23 12:20:28 +0200

    fixes

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ def printLine(text):
 printLine("Getting the most popular three articles of all time")
 postsList = getTopPosts()
 for post in postsList:
-    printLine("- " + post[0] + " - " + post[1] + " views")
+    printLine("- " + post[0] + " - " + str(post[1]) + " views")
 
 printLine("Getting the most popular article authors of all time")
 authorsList = getTopAuthors()
 for author in authorsList:
-    printLine("- " + author[0] + " - " + author[1] + " views")
+    printLine("- " + author[0] + " - " + str(author[1]) + " views")
 
 printLine("Getting which days did more than 1% of requests lead to errors")
 datesList = getTopErrorDays()
 for date in datesList:
-    printLine("- " + str(date[0].now().date()) + " - " + date[1] + " views")
+    printLine("- " + str(date[0].now().date()) + " - " + str(date[1]) + " %")
